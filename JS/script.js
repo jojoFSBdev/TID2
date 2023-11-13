fe219a37ce16c6ac440a1ba46c11cc2f6d875c66
jojoFSB.dev <joaquimfsb.si@gmail.com>
2023-11-13 13:07:21 -0300

    aaaaaa

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+var listaRelatos = [];
+
 function NovoRelato(){
     document.querySelector(".novo_relato").style.display = "flex";    
 }
@@ -7,17 +9,23 @@ function Cancelar(){
     
     var titulo = document.getElementById("entrada_titulo").value = null;
     var texto = document.getElementById("entrada_corpo").value = null;
+    var codinome = document.getElementById("entrada_codinome").value = null
 }
 
 function EnviarRelato(){
     var titulo = document.getElementById("entrada_titulo").value;
     var texto = document.getElementById("entrada_corpo").value;
     var codinome = document.getElementById("entrada_codinome").value;
+    
+    var contagem = listaRelatos.length;
 
     var a_relato = document.createElement('a');
-    a_relato.setAttribute("id", "relato");
-    a_relato.setAttribute("onclick", "ExpandirRelato()");
+    a_relato.setAttribute("class", "relato");
+    a_relato.setAttribute("id", "relato_" + (contagem));
+    a_relato.setAttribute("onclick", "ExpandirRelato("+(contagem)+")");
+
     var div_relato = document.createElement('div');
+    div_relato.setAttribute("class", "div_relato_" + contagem)
 
     var titulo_relato = document.createElement('h2');
     var node_titulo = document.createTextNode(titulo)
@@ -36,11 +44,18 @@ function EnviarRelato(){
     div_relato.appendChild(codinome_relato);
     a_relato.appendChild(div_relato);
 
+    listaRelatos.push(a_relato);
+
+    console.log(listaRelatos);
+    console.log(a_relato);
+
+    console.log(listaRelatos.length)
+
     document.querySelector(".relato_conteudo").appendChild(a_relato);
     
     Cancelar();
 }
 
-function ExpandirRelato(){
-    window.alert("FOI PORRA!");
+function ExpandirRelato(num){
+    document.querySelector("div_relato_"+ (num)).style.height = "auto";
 }
